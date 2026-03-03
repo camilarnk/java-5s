14964cc18b642dd91de97e10dbd42ff8c8162521
Camila <reinkecamila@gmail.com>
2026-03-03 20:37:53 -0300

    Update EstudanteRepository.java

--- EstudanteRepository.java
+++ EstudanteRepository.java
@@ -3,6 +3,6 @@ package com.bn.school.repositories;
 import com.bn.school.models.EstudanteModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// JpaRepository<Model, TipoDoID>
+// JpaRepository<Model, TipoDoID> - T de entidade
 public interface EstudanteRepository extends JpaRepository<EstudanteModel, Long> {
 }
