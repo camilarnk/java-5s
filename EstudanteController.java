342eb608f749847623c39a97546dd55b71909452
Camila <reinkecamila@gmail.com>
2026-03-09 22:15:31 -0300

    get post delete status

--- EstudanteController.java
+++ EstudanteController.java
@@ -2,9 +2,14 @@ package com.bn.school.controllers;
 
 import com.bn.school.models.EstudanteModel;
 import com.bn.school.services.EstudanteService;
+import jakarta.servlet.Servlet;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,8 +22,9 @@ public class EstudanteController {
     private EstudanteService estudanteService;
 
     @GetMapping // informar que sera apenas uma busca
-    public List<EstudanteModel> findAll() {
-        return estudanteService.findAll();
+    public ResponseEntity<List<EstudanteModel> > findAll() {
+        List<EstudanteModel> request = estudanteService.findAll();
+        return ResponseEntity.ok().body(request);
     }
 
     @GetMapping("/{id}")
@@ -28,19 +34,26 @@ public class EstudanteController {
     }
 
     @PostMapping // informar que é um metodo de criaçao
-    public EstudanteModel criarEstudante(@RequestBody EstudanteModel estudanteModel) { // RequestBody para informar que a requisicao HTTP sera pelo corpo
-        return estudanteService.criarEstudante(estudanteModel);
+    public ResponseEntity<EstudanteModel> criarEstudante(@RequestBody EstudanteModel estudanteModel) { // RequestBody para informar que a requisicao HTTP sera pelo corpo
+        EstudanteModel request = estudanteService.criarEstudante(estudanteModel);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{id}")
+                .buildAndExpand(request.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(request);
     }
 
     @PutMapping("/{id}")
-    public EstudanteModel atualizarEstudante(@PathVariable Long id, @RequestBody EstudanteModel estudanteModel) { // o estudante sera passado pelo body
-        return estudanteService.atualizarEstudante(id, estudanteModel);
+    public ResponseEntity<EstudanteModel> atualizarEstudante(@PathVariable Long id, @RequestBody EstudanteModel estudanteModel) { // o estudante sera passado pelo body
+        EstudanteModel request = estudanteService.atualizarEstudante(id, estudanteModel);
+        return ResponseEntity.status(201).body(estudanteModel);
     }
 
     @DeleteMapping("/{id}")
     // PathVariable para extrair valores da **URL** e passar como parametro para o metodo
-    public void deletarEstudante(@PathVariable Long id) {
+    public ResponseEntity<?> deletarEstudante(@PathVariable Long id) {
         estudanteService.deletarEstudante(id);
+        return ResponseEntity.noContent().build(); // retornando um status
     }
 
 }
